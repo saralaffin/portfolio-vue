3be9bdf85978ea90b5f3ed577d5b2a571be8b538
Sara Laffin <sarala@umich.edu>
2020-04-15 00:43:05 -0400

    second draft of background content, problem solver theme

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -3,21 +3,36 @@
     <h2>
       I'm a problem solver
     </h2>
-    <p>
-      I've always been drawn to fixing and creating. As a child, I enjoyed
-      building things with my dad. In high school, I fell in love with modular
-      origami. <br />
-      I studied marine engineering in college, earning my Masters and Bachelors
-      from the University of Michigan. I've held a variety of roles in the
-      marine engineering field, mainly in the form of internships. <br />
-      Next, I tried something more hands-on. 'Sharpened a different saw', if you
-      will. I spent 3 seasons with Americorps, mainly on chainsaw crews, out in
-      Colorado. <br />
-      Now I'm returning to my engineering roots, hopefully with a mix of
-      hard-work, hands-on, challenging projects.
-    </p>
-    <div class="link-container">
-      <a href="#skills">Skills</a>
+    <div class="text-container">
+      <p>
+        I've always been drawn to fixing and creating. As a child, I enjoyed
+        building things with my dad. In high school, I fell in love with modular
+        origami. As you may be aware, any building endeavor comes with a set of
+        problems to solve.
+      </p>
+      <p>
+        I studied marine engineering in college, earning my Masters and
+        Bachelors from the University of Michigan. The problem sets, labs, and
+        projects were seemingly endless, but success came in the form of all
+        A's, Summa Cum Laude.
+      </p>
+      <p>
+        I've held a variety of roles in the marine engineering field, mainly in
+        the form of internships. After studidying and working almost non-stop
+        for 5 years, I had a new problem to solve: burn-out.
+      </p>
+      <p>
+        Next, I tried something more hands-on. 'Sharpened a different saw', if
+        you will. I spent 3 seasons with Americorps, mainly on chainsaw crews
+        out in Colorado. Running a chainsaw, and leading crews, presented their
+        own obstacles that helped me grow and solve problems in unimaginable
+        ways.
+      </p>
+      <p>
+        Now I'm returning to my engineering roots in the form of a software
+        engineer! My dream is to continue to build (and fix) challenging
+        projects. What solutions can I help you find?
+      </p>
     </div>
   </div>
 </template>
@@ -40,12 +55,16 @@ export default {
 }
 h2 {
   color: var(--white);
+  opacity: 1;
 }
-p {
+.text-container {
   text-align: center;
-  padding: 1em;
   margin-top: 4vh;
   margin-bottom: 10vh;
+  color: var(--white);
+}
+p {
+  padding: 1em;
 }
 .link-container {
   display: flex;
